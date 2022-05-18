b0e3612076cdbbaac3ae31d14228950287cd96c3
Guli-Alimova <86463317+Guli-Alimova@users.noreply.github.com>
2022-05-18 11:32:51 +0500

    task

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,93 @@
 <template>
-<h1>hello world</h1>
+<div class="container">
+<Header title="track"/>
+<Tasks :tasks="tasks"/>
+</div>
+
 </template>
 
 <script>
-
-
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
 export default {
   name: 'App',
+  components:{
+    Header,
+    Tasks
+  },
+  data(){
+    return{
+      tasks:[]
+    }
+  },
+  created(){
+    this.tasks = [
+    {
+      id:1,
+      text:'Liverpool was created',
+      day:'march 1st march 1999 at 2:30pm',
+      reminder:true
+    },
+     {
+      id:2,
+      text:'Barcelona was created',
+      day:'January 2st march 1999 at 1:30pm',
+      reminder:true
+    },
+     {
+      id:3,
+      text:'Chalsea was created',
+      day:'march 1st march 1989 at 2:30pm',
+      reminder:true
+    }
+    ]
+  }
 
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&family=Poppins:wght@400;500;600;700&display=swap');
 
+*{
+box-sizing:border-box;
+padding:0;
+margin:0;
+}
+body{
+  font-family: 'Poppins', sans-serif;
+}
+.container{
+  max-width: 500px;
+  margin:30px auto;
+  overflow:auto;
+  min-height:300px;
+  border:1px solid steelblue;
+  padding:30px;
+  border-radius:5px;
+}
+.btn{
+display:inline-block;
+background: #000;
+color:#fff;
+border:none;
+padding:10px 20px;
+margin:5px;
+border-radius:5px;
+cursor:pointer;
+text-decoration:none;
+font-size: 15px;
+font-family: inherit;
+}
+.btn:focus{
+  outline:none;
+
+}
+.btn:active{
+  transform:scale(0.98);
+}
+.btn-block{
+  display:block;
+  width: 100%;
+}
 </style>
